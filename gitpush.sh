1f9e7f761cbd1d5bbf993f6da84be3fee26a19f1
Amir5696 <www.amirj26670@gmail.com>
2021-03-23 09:27:01 +0000

    Enable Django admin for user profile model

--- gitpush.sh
+++ gitpush.sh
@@ -5,6 +5,8 @@ then
 else
 	git config --global user.email "www.amirj26670@gmail.com"
   git config --global user.name "Amir5696"
+  git config --global user.username "amir5696"
+  git config --global user.password "@Mir6670"
 	git add . 
 	git commit -am "$1"
 	git push origin
