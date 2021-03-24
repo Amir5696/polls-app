321cd440173a7ba69c9c3e05c8624f40e1764887
amir6670 <www.amirj26670@gmail.com>
2021-03-24 21:00:58 +0430

    Adding polls app to admin panel

---
+++ polls/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.Question)
+
+# Register your models here.
---
+++ polls/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hey Amir :)")
+
+
+def details(request, question_id):
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    output = ",".join([q.question_text for q in latest_questions])
+    return HttpResponse(output)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
